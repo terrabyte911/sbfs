fa1d0ec280632b933d233ccbe142b79feb9573d7
terrabyte911 <davidclarkeace@yahoo.co.uk>
2018-07-07 22:35:27 +0100

    Update 1530902727.cmd

--- webmin/custom/1530902727.cmd
+++ webmin/custom/1530902727.cmd
@@ -1,5 +1,5 @@
 /etc/seedbox-from-scratch/vpnadduser $name $pass
 vpn add user
 root 0 0 0 0 0 0 0 -
-name:0::0,1:Client Name
-pass:8::0,0:Password
+name:0::0,1:Client Name (Required)
+pass:8::0,0:Zip Password (Optional)
