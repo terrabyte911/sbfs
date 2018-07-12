2e569bc6fec57107e965944f2573456a021a742f
terrabyte911 <davidclarkeace@yahoo.co.uk>
2018-07-12 21:45:00 +0100

    Update 1530902727.cmd

--- webmin/custom/1530902727.cmd
+++ webmin/custom/1530902727.cmd
@@ -1,5 +1,6 @@
-/etc/seedbox-from-scratch/vpnadduser $name $pass
+/etc/seedbox-from-scratch/vpnadduser $name $pass $batfile
 vpn add user
-root 0 0 0 0 0 0 0 -
-name:0::0,1:Client Name (Required)
-pass:8::0,0:Zip Password (Optional)
+root 0 0 0 1 0 0 0 -
+name:0::1,1:Client Name (Required)
+pass:8::1,0:Zip Password (Optional)
+batfile:7:1:1,1:Add OpenVPN .bat files
